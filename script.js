cf01f1a207fe078a0a8ca9001a942e1c80a55d9c
Matt Cluff <matthewrcluff@gmail.com>
2019-12-05 18:54:03 -0700

    COMPLETELY overhauled my html, css & javascript

--- script.js
+++ script.js
@@ -1,98 +1,78 @@
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
-const clipboard = document.getElementById('clipboard');
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "~", "`", "?", ".", ";", ":", ">", "<", "/", ",",];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-const randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol
+    let user = [];
+    let specialCharactersArray;
+    let numbersArray;
+    let upperCaseArray;
+    let lowercaseArray;
+    let randomNumber = 0;
+    let passwordLength = 0;
+    let endpassword = "";
+
+
+    const generatePasswordStart = document.getElementById("genStart");
+    generatePasswordStart.addEventListener("click", password);
+
+    function password() {
+        let passwordLength = prompt("How long would you like your password? Choose a number between 8 and 128.");
+
+        if (passwordLength <= 7 || passwordLength >= 129) {
+            passwordLength = prompt("Error! Try again. Choose a number for your password between 8 and 128 characters.");
+        }
+
+        const upperConfirm = confirm("Do you need any uppercase letters?");
+        let lowerConfirm;
+        if (upperConfirm) {
+            lowerConfirm = confirm("Got it! Would you like any lowercase letters?");
+    
+        } else if (!upperConfirm) {
+            lowerConfirm = confirm("Okay, would you like any lowercase characters?");
+        }
+
+        SpecialConfirm = confirm ("Would you like any special characters?");
+        const numbersConfrim = confirm("Got it! Would you like any numbers in your password?");
+
+        if (numbersConfrim || SpecialConfirm || upperConfirm || lowerConfirm) {
+            alert("Magically creating your custom password now");
+        } else {
+            alert("You need to select at least one option to make the magic happen! Please ask for an uppercase, lowercase, special character, or number.");
+            password();
+        }
+        
+        finalpassword();
+
+        function finalpassword() {
+        if (SpecialConfirm) {
+            specialCharactersArray = user.concat(specialCharacters)
+            user = specialCharactersArray;
+        };
+        if (upperConfirm) {
+            upperCaseArray = user.concat(uppercase);
+            user = upperCaseArray;
+        };
+        if (lowerConfirm) {
+            lowercaseArray = user.concat(lowercase);
+            user = lowercaseArray;
+        };
+        if (numbersConfrim) {
+            numbersArray = user.concat(numbers);
+            user = numbersArray;
+        };
+
+        for (let i = 0; i < passwordLength; i++) {
+            randomNumber = Math.floor(Math.random()*user.length);
+            endpassword += user[randomNumber];
+            document.getElementById("pass").value = endpassword;
+        }
+
+        document.getElementById("genStart").onclick = function() {password()};
+    }
 }
 
-clipboard.addEventListener('click', () => {
-	const textarea = document.createElement('textarea');
-	const password = resultEl.innerText;
-	
-	if(!password) { return; }
-	
-	textarea.value = password;
-	document.body.appendChild(textarea);
-	textarea.select();
-	document.execCommand('copy');
-	textarea.remove();
-	alert('Password copied to clipboard');
-});
-
-generate.addEventListener('click', () => {
-	const length = +lengthEl.value;
-	const hasLower = lowercaseEl.checked;
-	const hasUpper = uppercaseEl.checked;
-	const hasNumber = numbersEl.checked;
-	const hasSymbol = symbolsEl.checked;
-	
-	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-});
-
-function generatePassword(lower, upper, number, symbol, length) {
-	let generatedPassword = '';
-	const typesCount = lower + upper + number + symbol;
-	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-	
-	// Doesn't have a selected type
-	if(typesCount === 0) {
-		return '';
-	}
-	
-	// create a loop
-	for(let i=0; i<length; i+=typesCount) {
-		typesArr.forEach(type => {
-			const funcName = Object.keys(type)[0];
-			generatedPassword += randomFunc[funcName]();
-		});
-	}
-	
-	const finalPassword = generatedPassword.slice(0, length);
-	
-	return finalPassword;
-}
-
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-
-
-
-
-
-
-// SOCIAL PANEL JS
-const floating_btn = document.querySelector('.floating-btn');
-const close_btn = document.querySelector('.close-btn');
-const social_panel_container = document.querySelector('.social-panel-container');
-
-floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
-});
-
-close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
-});
+document.getElementById("copyToClipboard").addEventListener("click", ()=> {
+    document.getElementById("pass").select();
+    document.execCommand("copy");
+})
